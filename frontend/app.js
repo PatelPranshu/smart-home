93cc48a6b67bab6f9711c56c692723d5600e8a39
Pranshu Patel <pranshuvramani@gmail.com>
2025-12-19 02:28:16 +0530

    1.2.2 bug fix url

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,5 @@
 const API_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
-    ? 'http://localhost:5000/api'
+    ? 'http://localhost:3000/api'
     : 'https://smarthome-backend-rbmc.onrender.com/api';
 
 // --- DOM REFS ---
